c94d44121b05b459b6a6f4dc11e86cf227cdecb5
Wesley Marques <wes.academico@gmail.com>
2020-06-02 23:04:18 -0300

    first commit

---
+++ backend/src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import routes from "./routes";
+import path from "path";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3000);
+
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
